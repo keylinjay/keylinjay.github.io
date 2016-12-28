eece2b8a0037c905d171b78aa61ee8f495c8f263
keylinjay <k547531366@hotmail.com>
2016-12-28 22:14:16 +0800

    update kQuery.js, from macair

--- assets/js/kquery.js
+++ assets/js/kquery.js
@@ -96,6 +96,109 @@ kQuery.fn = kQuery.prototype = {
                  return callback.call( elem, i, elem );
              }));
          },
+    slice: function() {
 
+               return this.pushStack( slice.apply( this, arguments ) );
+           },
 
+    first: function() {
+               return this.eq( 0 );
+           },
+
+    last: function() {
+              return this.eq( -1 );
+          },
+
+    eq: function( i ) {
+            var len = this.length,
+                j = +i + ( i < 0 ? len : 0 );
+            return this.pushStack( j >= 0 && j < len ? [ this[j] ] : [] );
+        },
+
+    end: function() {
+             return this.prevObject || this.constructor( null );
+         },
+
+    push: push,
+    sort: arr.sort,
+    splice: arr.splice,
+
+};
+
+kQuery.extend = kQuery.fn.extend = function() {
+    var options, name, src, copy, copyIsArray, clone,
+        target = arguments[ 0 ] || {},
+        i = 1,
+        length = arguments.length,
+        deep = false;
+
+    if ( typeof target === "boolean" ) {
+        deep = target;
+        target = arguments[ 1 ] || {};
+        i++;
+    }
+
+    if ( typeof target !== "object" && !kQuery.isFunction( target ) ) {
+        target = {};
+    }
+
+    if ( i === length ) {
+        target = this;
+        i--;
+    }
+
+    for ( ; i < length; i++ ) {
+        if ( (options = arguments[ i ]) != null ) {
+            for ( name in options ) {
+                src = target[ name ];
+                copy = options[ name ];
+
+                if ( target === copy ) {
+                    continue;
+                }
+
+                if ( deep && copy && ( kQuery.isPlainObject(copy) || (copyIsArray = kQuery.isArray(copy)) ) ) {
+                    if ( copyIsArray ) {
+                        copyIsArray = false;
+                        clone = src && kQuery.isArray(src) ? src : [];
+                    } else {
+                        clone = sec && kQuery.isPlainObject(src) ? src : {};
+                    }
+                    target[ name ] = kQuery.extend( deep, clone, copy );
+                } else if ( copy !== undefined ) {
+                    target[ name ] = copy;
+                }
+            }
+        }
+    }
+    return target;
+};
+
+kQuery.extend({
+
+    expando: "kQuery" + ( version + Math.random() ).replace( /\D/g, "" ),
+
+    isReady: true,
+
+    error: function( msg ) {
+        throw new Error( msg );
+    },
+
+    noop: function(){},
+
+    isFunction: function( obj ) {
+        return kQuery.type( obj ) === "function";
+    },
+
+    isArray: Array.isArray,
+
+    isWindow: function( obj ) {
+        return obj != null && obj === obj.window;
+    },
+
+    isNumeric: function( obj ) {
+                   return !kQuery.isArray( obj ) && (obj - parseFloat( obj ) + 1) >= 0;
+               },
+});
+};
 }));
